f80ccab0db2a03d16114d9faaed7535a5ac73ae4
Lorenzo Lombardi <l.lombardi@afbnet.it>
2020-02-01 11:41:57 +0100

    icons and init.sql modified

--- Docker/Postgres/init.sql
+++ Docker/Postgres/init.sql
@@ -1,5 +1,8 @@
+CREATE EXTENSION btree_gist;
+CREATE EXTENSION pgcrypto;
+
 CREATE TABLE public.libri (
-    id int PRIMARY KEY,
+    id serial PRIMARY KEY,
     codice varchar(10),
     ean13 varchar(20),
     titolo varchar(255),
@@ -10,10 +13,14 @@ CREATE TABLE public.libri (
     collana varchar(100),
     argomento varchar(30),
     linea_prodotto varchar(50),
-    disponibilita int
+    disponibilita int,
+    create_dttm timestamp,
+    mod_dttm timestamp,
+    create_id int,
+    mod_id int
 );
 CREATE TABLE public.audit_trail(
-    id int PRIMARY KEY,
+    id serial PRIMARY KEY,
     table_name varchar(50),
     column_name varchar(50),
     row_name int,
@@ -22,3 +29,20 @@ CREATE TABLE public.audit_trail(
     old_value varchar(255),
     new_value varchar(255)
 );
+CREATE TABLE public.users(
+    id serial PRIMARY KEY,
+    username varchar(100),
+    saltkey varchar(50),
+    userpwd varchar(100),
+    userrole varchar(20),
+    lastlogin_dttm timestamp,
+    userfirstname varchar(100),
+    userlastname varchar(100),
+    create_dttm timestamp,
+    mod_dttm timestamp,
+    create_id int,
+    mod_id int
+);
+INSERT INTO public.users (id,username,saltkey,lastlogin_dttm,userfirstname,userlastname,create_dttm,mod_dttm,create_id,mod_id) 
+VALUES (1,'theloz',gen_salt('bf'),NOW(),'Lorenzo', 'Lombardi',NOW(),NOW(),1,1);
+UPDATE public.users SET userpwd = crypt('strong!!password', (SELECT saltkey FROM public.users WEHRE id=1))  WHERE id=1;

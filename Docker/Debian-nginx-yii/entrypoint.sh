564215c1a9e8808e5078bc8d1371ffa4a13a7488
Lorenzo Lombardi <l.lombardi@afbnet.it>
2020-02-01 20:23:31 +0100

    Update entrypoint.sh

--- Docker/Debian-nginx-yii/entrypoint.sh
+++ Docker/Debian-nginx-yii/entrypoint.sh
@@ -42,7 +42,6 @@ else
 fi
 #Setting PHP
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.3/fpm/php.ini
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.3/cli/php.ini
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 256M/g" /etc/php/7.3/cli/php.ini
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 256M/g" /etc/php/7.3/fpm/php.ini
 
